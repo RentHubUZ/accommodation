90b6adde4a0cd6f5fec23bd39c693064d2a8828d
Ozodbek2129 <ozodbekshamsiddinov1504@gmail.com>
2024-09-25 15:45:01 +0500

    all

--- migrations/000002_create_property_image.up.sql
+++ migrations/000002_create_property_image.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "PropertyImages" (
   "id" UUID PRIMARY KEY DEFAULT (gen_random_uuid()),
-  "property_id" UUID NOT NULL,
+  "property_id" UUID REFERENCES properties(id) NOT NULL,
   "image_url" VARCHAR(255) NOT NULL,
   "uploaded_at" TIMESTAMP DEFAULT (NOW())
 );
